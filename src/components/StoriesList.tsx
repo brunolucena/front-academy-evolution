6a920b5697a65daac75ae9cd6260de09be89347b
Bruno Lucena <bruno.lucenac@gmail.com>
2022-04-19 22:05:30 -0300

    Classes condicionais e TypeScript opções

--- src/components/StoriesList.tsx
+++ src/components/StoriesList.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import StoryButton from "./StoryButton/StoryButton";
 
 export default function StoriesList() {
+  const [hasSeen, setHasSeen] = useState(false);
+
   return (
     <div>
+      <button onClick={() => setHasSeen(!hasSeen)}>
+        Toggle
+      </button>
+
       <StoryButton
         image="https://www.frontacademy.com.br/images/depoimentos/daniel.png"
         name="dani"
-        hasSeen
+        hasSeen={hasSeen}
       />
 
       <StoryButton
--- src/components/StoryButton/StoryButton.tsx
+++ src/components/StoryButton/StoryButton.tsx
@@ -1,21 +1,36 @@
 import UserIcon from "../UserIcon";
+import clsx from 'clsx';
 import "./StoryButton.scss";
 
 interface Props {
   hasSeen?: boolean;
   image: string;
   name?: string;
-  size?: string;
+  size?: 'lg' | 'md' | 'sm';
 }
 
 export default function StoryButton({
   hasSeen,
   image,
   name,
-  size = '20px',
+  size = 'lg',
 }: Props) {
   return (
-    <div className={"story-button-container " + (hasSeen && 'has-seen')}>
+    // <div className={"story-button-container " + (hasSeen && 'has-seen')}>
+    // <div className={clsx('story-button-container', 'has-seen')}> // clsx sem condições
+    // <div className={clsx('story-button-container', hasSeen && 'has-seen')}> clsx com condição de string
+    // <div className={clsx({ // clsx com objetos
+    //   'story-button-container': true,
+    //   'has-seen': hasSeen,
+    // })}>
+    // clsx com array - que é o Milhor
+    <div className={clsx([
+      'story-button-container',
+      hasSeen && 'has-seen',
+      size === 'lg' && 'lg',
+      size === 'md' && 'md',
+      size === 'sm' && 'sm',
+    ])}>
       <div className="user-icon-container">
         <UserIcon image={image} />
       </div>
